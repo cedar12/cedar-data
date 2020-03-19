4a4931b98a181b5d123354491dc21284d09c8303
cedar12 <413338772@qq.com>
2020-03-19 10:03:23 +0800

    fix code

--- src/main/java/cn/cedar/Main.java
+++ src/main/java/cn/cedar/Main.java
@@ -5,16 +5,12 @@ import cn.cedar.data.HandleFactory;
 
 public class Main {
 
-    public static void p(Object obj){
-        System.out.println(obj);
-    }
-
     public static void main(String[] args)  {
         HandleFactory factory=new HandleFactory();
         ADao d= (ADao) factory.getInstance(ADao.class);
 
         long time=System.currentTimeMillis();
-        d.selectDto().forEach(Main::p);
+        d.selectDto().forEach(System.out::println);
         System.out.println("耗时："+(System.currentTimeMillis() - time));
         System.out.println("===========");
         time=System.currentTimeMillis();
