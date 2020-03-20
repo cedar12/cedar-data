620e1f82de17b8afd34e4ca8a45515636fb51c83
cedar12 <413338772@qq.com>
2020-03-20 15:12:47 +0800

    新增 1.扩展获取Connection 2.插入语句可获取自增长id

--- src/main/java/cn/cedar/JdbcExtension.java
+++ src/main/java/cn/cedar/JdbcExtension.java
@@ -10,7 +10,7 @@ public class JdbcExtension extends JdbcUtil {
 
     @Override
     public Connection getConnection() {
-        System.out.println("不使用jdbc连接。。。");
+        System.out.println("不使用默认jdbc连接。。。");
         try {
             if(isClosed()){
                 connection=DriverManager.getConnection(getUrl(),getUser(),getPassword());
